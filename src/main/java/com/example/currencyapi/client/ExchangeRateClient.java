9cf01155641075ec73380741b187880b8eedfe53
ISWEAVER26 <isaacweaver54@gmail.com>
2026-04-17 12:10:50 +0100

    Added call to external API (td add API key env var)

--- src/main/java/com/example/currencyapi/client/ExchangeRateClient.java
+++ src/main/java/com/example/currencyapi/client/ExchangeRateClient.java
@@ -1,10 +1,27 @@
 package com.example.currencyapi.client;
 
 import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.function.client.WebClient;
+import java.util.Map;
 
 @Component 
 public class ExchangeRateClient {
-    public double getRate(String from, String to) {
-        return 1.0;
+
+    private final WebClient webClient;
+
+    public ExchangeRateClient(WebClient webClient) {
+        this.webClient = webClient; 
     }
+
+
+    public double getRate(String from, String to) {
+        Map <String, Object> response = webClient.get( )
+                            .uri("https://v6.exchangerate-api.com/v6/API_KEY/latest/{from}", from)
+                            .retrieve()
+                            .bodyToMono(Map.class)
+                            .block();
+
+        Map <String, Double> conversionRates = (Map <String, Double>) response.get("conversion_rates");
+        return conversionRates.get(to);
+    }   
 }
